69cd2e15aeafa9bae18c119fabba5017cdf882da
Krzysztof Pytel <37655632+krzysztof97@users.noreply.github.com>
2019-01-20 19:22:48 +0100

    Initial commit

---
+++ BattleshipCore/Battleship.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattleshipCore
+{
+    public class Battleship
+    {
+        public Battleship()
+        {
+
+        }
+    }
+}
