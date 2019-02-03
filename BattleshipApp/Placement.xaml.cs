b488a6ab7db47b17855883ab59ce8da75ff97b9c
Krzysztof Pytel <37655632+krzysztof97@users.noreply.github.com>
2019-02-03 23:22:12 +0100

    Work with click events

--- BattleshipApp/Placement.xaml.cs
+++ BattleshipApp/Placement.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BattleshipCore.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,7 +24,44 @@ namespace BattleshipApp
         public Placement()
         {
             InitializeComponent();
-            Console.WriteLine("laduje");
+            PlacementGrid.MouseDown += new MouseButtonEventHandler(PlacementGridClick);
         }
+        private void PlacementGridClick(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton.ToString() != "Left")
+            {
+                return;
+            }
+            var point = Mouse.GetPosition(PlacementGrid);
+
+            int row = 0;
+            int col = 0;
+            double accumulatedHeight = 0.0;
+            double accumulatedWidth = 0.0;
+
+            // calc row mouse was over
+            foreach (var rowDefinition in PlacementGrid.RowDefinitions)
+            {
+                accumulatedHeight += rowDefinition.ActualHeight;
+                if (accumulatedHeight >= point.Y)
+                    break;
+                row++;
+            }
+
+            // calc col mouse was over
+            foreach (var columnDefinition in PlacementGrid.ColumnDefinitions)
+            {
+                accumulatedWidth += columnDefinition.ActualWidth;
+                if (accumulatedWidth >= point.X)
+                    break;
+                col++;
+            }
+
+            Console.WriteLine(row);
+            Console.WriteLine(col);
+            Console.WriteLine(OrientationBox.SelectionBoxItem);
+        }
+        
+
     }
 }
