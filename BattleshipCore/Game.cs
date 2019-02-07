a60db5e6c61ca3e77ecb7b904c3a2c21d61f9b8f
Krzysztof Pytel <37655632+krzysztof97@users.noreply.github.com>
2019-02-07 22:23:44 +0100

    Add ShipPlacement instance to GameEngine

--- BattleshipCore/Game.cs
+++ BattleshipCore/Game.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BattleshipCore.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,9 +9,16 @@ namespace BattleshipCore
 {
     public class Game
     {
+        private ShipPlacement shipPlacement;
+
         public Game()
         {
+            shipPlacement = new ShipPlacement();
+        }
 
+        public bool ShipDeploy(Ship ship, int xPos, int yPos)
+        {
+            return shipPlacement.ShipDeploy(ship, xPos, yPos);
         }
     }
 }
