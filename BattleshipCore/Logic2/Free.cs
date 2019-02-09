2baf00f8a92459a94e2c980580053f524eb46f47
Jacek Bebak <jacek.a.bebak@gmail.com>
2019-02-09 23:24:29 +0100

    Fuck...

--- BattleshipCore/Logic2/Free.cs
+++ BattleshipCore/Logic2/Free.cs
@@ -6,23 +6,90 @@ using System.Threading.Tasks;
 
 namespace BattleshipCore.Models
 {
-    class Free
+    class Free : IComparable
     {
-        public static bool IsFree(int min, int max, int value)
+        public static bool IsFree(Ship aship, Ship ship)
         {
             bool isFree = true;
 
-            for (int i = min; i < max; i++)
+
+            switch (aship.Orientation)
             {
-                if (i == value)
-                {
-                    isFree = false;
-                }
-                else
-                    isFree = true;
+                case OrientationEnum.Horizontal:
+                    for (int i = aship.XPos-1; i <= (aship.XPos + aship.Size); i++)
+                    {
+                        for (int j = ship.XPos-1; j <= (ship.XPos + ship.Size); j++)
+                        {
+                            if (i == j)
+                            {
+                                isFree = false;
+                            }
+                            else
+                                isFree = true;
+                        }
+                    }
+                    break;
+                case OrientationEnum.Vertical:
+                    for (int i = aship.YPos-1; i <= (aship.YPos + aship.Size); i++)
+                    {
+                        for (int j = ship.YPos-1; j <= (ship.YPos + ship.Size); j++)
+                        {
+                            if (i == j)
+                            {
+                                isFree = false;
+                            }
+                            else
+                                isFree = true;
+                        }
+                    }
+                    break;
             }
+            switch (ship.Orientation)
+            {
+                case OrientationEnum.Horizontal:
+                    for (int i = aship.XPos-1; i <= (aship.XPos + aship.Size); i++)
+                    {
+                        for (int j = ship.XPos-1; j <= (ship.XPos + ship.Size); j++)
+                        {
+                            if (i == j)
+                            {
+                                isFree = false;
+                            }
+                            else
+                                isFree = true;
+                        }
+                    }
+                    break;
+                case OrientationEnum.Vertical:
+                    for (int i = aship.YPos-1; i <= (aship.YPos + aship.Size); i++)
+                    {
+                        for (int j = ship.YPos-1; j <= (ship.YPos + ship.Size); j++)
+                        {
+                            if (i == j)
+                            {
+                                isFree = false;
+                            }
+                            else
+                                isFree = true;
+                        }
+                    }
+                    break;
+            }
+            //for (int i = min; i < max; i++)
+            //{
+            //    if (i == value)
+            //    {
+            //        isFree = false;
+            //    }
+            //    else
+            //        isFree = true;
+            //}
             return isFree;
         }
 
+        public int CompareTo(object obj)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
